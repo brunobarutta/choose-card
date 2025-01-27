1dbbe91f00440a6bb53f96faceafec2efd3d535a
brunobarutta <brunocesarbarutta@gmail.com>
2025-01-27 18:03:58 -0300

    criando arqquivo JS e criando variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+let imagem1 = document.querySelector('img#img-1')
+let imagem2 = document.querySelector('img#img-2')
+let imagem3 = document.querySelector('img#img-3')
+let imagem4 = document.querySelector('img#img-4')
+let todasImg = [imagem1 , imagem2 , imagem3 , imagem4];
+let sobreMim = document.querySelector('i#sobre-mim')
+let skills = document.querySelector('i#skills')
+let projects = document.querySelector('i#projects')
+let contact = document.querySelector('i#contato')
+let todosIcones = [sobreMim , skills , projects , contact];
+
